ed9fdbc7184647cd4938af18df89770eac2c6a34
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:52:09 +0200

    Add files via upload
    Python basic program where input variables are printed out.

---
+++ input_print_output.py
@@ -0,0 +1,12 @@
+#Program takes the users favourite restaurant using input and favourite number
+fav_rest = input("Whats your favourite restaurant amigo: ")
+
+int_fav = input("Whats your favourite number: ")
+
+int_fav = int(int_fav)
+
+#Prints out the variables
+print(fav_rest)
+
+print(int_fav)
+
