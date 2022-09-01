510f95b2956f5a45a958d2beae6b7badf6046ebe
Obies <41453896+Obies@users.noreply.github.com>
2022-09-02 00:52:49 +0200

    Add files via upload
    Python basic program uses dictionary and prints out the value of key.

---
+++ key_value.py
@@ -0,0 +1,5 @@
+#Program prints capital city of different countries
+countryMap = {'Ethiopia': 'Addis Ababa', 'Madagascar': 'Antananarivo', 'Mauritius': 'Port Louis'}
+
+#the command below returns the capital city of the country entered
+print(countryMap['Mauritius'])
