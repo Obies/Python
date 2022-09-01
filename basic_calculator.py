7301f0f81dcb624cde8c1f3784150f57d9e07cb1
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 20:01:27 +0200

    Add files via upload
    Basic Python program to perform basic calculation.

---
+++ basic_calculator.py
@@ -0,0 +1,22 @@
+#Program requests integers from user and prints out the different calculations
+num1 = int(input("Please enter the first number champ: "))
+
+num2 = int(input("Please enter the second number champ: "))
+
+num3 = int(input("Please enter the third number champ: "))
+
+sum = num1 + num2 + num3
+
+minus = (num1 - num2)
+
+multiply = (num3 * num1)
+
+division = (sum / num3)
+#Prints out the different calculations
+print(sum)
+
+print(minus)
+
+print(multiply)
+
+print(division)
