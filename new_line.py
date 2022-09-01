ca38d3c02a7d60199bf12f966ead73c39125631a
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 22:51:08 +0200

    Add files via upload
    Python basic program uses newline method to print a sentence in new lines.

---
+++ new_line.py
@@ -0,0 +1,5 @@
+#Program prints each sentence word on a new line using replace method
+sentence = input("Enter your wish: ")
+
+#replace method replaces the spaces between the sentence with a new line function
+print(sentence.replace(" ", "\n"))
