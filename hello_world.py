a667f5ae714b317aa19f48729725050c9f816acc
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 15:53:31 +0200

    Add files via upload
    Basic program to create hello world in the Python language.

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+#This code asks the user for their name and age and prints them out
+name = input("Please type your name champ: ")
+print(name)
+age = input("Please enter your age champ: ")
+print(age)
+#prints out a new line and then the statement
+print("\nHello World!")
