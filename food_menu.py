6cf2e978c2c98a7544867873d1058608e856097c
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:04:58 +0200

    Add files via upload
    Python basic program asks user food to order and prints out their items

---
+++ food_menu.py
@@ -0,0 +1,7 @@
+#program asks users to order 3 food items using input function
+item1 = input("Whats your first favourite food: ")
+item2 = input("Whats your second favourite food: ")
+item3 = input("Whats your third favourite food: ")
+#prints out the entered items from the user
+print("Order confirmed! You receive:\n {} \n {} \n {}".format(item1, item2, item3))
+
