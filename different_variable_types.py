3db49e6ba0a0b3553fc4d7bea39e8be8d8d2e658
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:32:02 +0200

    Add files via upload
    Python basic program showing the different variable types used.

---
+++ different_variable_types.py
@@ -0,0 +1,21 @@
+#This program declares and assigns values for the different variable types
+int = 5
+
+string = "You are awesome"
+
+char = "C"
+
+float = "777.77"
+
+boolean = True
+
+#The values are then printed out
+print(int)
+
+print(string)
+
+print(char)
+
+print(float)
+
+print(boolean)
