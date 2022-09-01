af6f2c2b84d880b70ae2e80ae934b81a3012c0cf
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:09:43 +0200

    Add files via upload
    Python basic program to convert 1 data type to another

---
+++ converting_data_types.py
@@ -0,0 +1,20 @@
+#This program converts one data type to another using casting
+num1 = 99.23
+
+num2 = 23
+
+num3 = 150
+
+string1 = "100"
+
+#casting converts the data type to another
+num1_int = int(num1)
+
+num2_float = float(num2)
+
+num3_str = str(num3)
+
+string1_int = int(string1)
+
+#prints out the converted data types
+print("\n {} \n {} \n {} \n {}".format(num1_int, num2_float, num3_str, string1_int))
