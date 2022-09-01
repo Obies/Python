2dbb1a4ead5e7e4cc45423152cfba2fce9cca655
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:12:21 +0200

    Add files via upload
    Python basic program to get input from user and print it out.

---
+++ input_method.py
@@ -0,0 +1,11 @@
+#This program requests details from the user using input function
+name = input("Please enter your cool name: ")
+
+age = input("Please enter your age: ")
+
+house_number = input("Please enter your castle number: ")
+
+street_name = input("Please enter your street name: ")
+
+#prints out all the details in one sentence
+print("This is {} he is {} years old and lives at house number {} on {} Street.".format(name, age, house_number, street_name))
