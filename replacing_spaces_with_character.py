49e811466654b77c69221e1feaf1bd09e665b965
Obies <41453896+Obies@users.noreply.github.com>
2022-09-01 16:43:36 +0200

    Add files via upload
    Python basic program to use replace method to insert a specific character.

---
+++ replacing_spaces_with_character.py
@@ -0,0 +1,9 @@
+#Program uppercases the variable string and adds special characters
+hero = "Super Man"
+
+#Uses inline python methods
+hero = hero.replace("", "^").upper().replace("^ ^", " ")
+
+print(hero[1:-1])
+
+
